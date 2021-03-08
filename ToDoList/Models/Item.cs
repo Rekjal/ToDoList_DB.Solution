fb5e8eaf8cc3e6bda416fc3f503e013813d750ce
svayalk <32828154+Rekjal@users.noreply.github.com>
2021-03-08 13:58:16 -0800

    add Find() method
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -14,9 +14,10 @@ namespace ToDoList.Models
     }
     public Item(string description, int id)
     {
-        Description = description;
-        Id = id;
+      Description = description;
+      Id = id;
     }
+
     public override bool Equals(System.Object otherItem)
     {
       if (!(otherItem is Item))
@@ -32,6 +33,58 @@ namespace ToDoList.Models
       }
     }
 
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+
+      cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@ItemDescription";
+      description.Value = this.Description;
+      cmd.Parameters.Add(description);    
+      cmd.ExecuteNonQuery();
+      Id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static Item Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM `items` WHERE id = @thisId;";
+
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+      cmd.Parameters.Add(thisId);
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemDescription = "";
+      while (rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemDescription = rdr.GetString(1);
+      }
+      Item foundItem= new Item(itemDescription, itemId);
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundItem;
+    }
+
     public static List<Item> GetAll()
     {
       List<Item> allItems = new List<Item> { };
@@ -41,17 +94,17 @@ namespace ToDoList.Models
       cmd.CommandText = @"SELECT * FROM items;";
       MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
       while (rdr.Read())
-        {
-            int itemId = rdr.GetInt32(0);
-            string itemDescription = rdr.GetString(1);
-            Item newItem = new Item(itemDescription, itemId);
-            allItems.Add(newItem);
-        }
+      {
+          int itemId = rdr.GetInt32(0);
+          string itemDescription = rdr.GetString(1);
+          Item newItem = new Item(itemDescription, itemId);
+          allItems.Add(newItem);
+      }
       conn.Close();
       if (conn != null)
-        {
-            conn.Dispose();
-        }
+      {
+          conn.Dispose();
+      }
       return allItems;
     }
 
@@ -59,41 +112,14 @@ namespace ToDoList.Models
     {
       MySqlConnection conn = DB.Connection();
       conn.Open();
-      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      var cmd = conn.CreateCommand() as MySqlCommand;
       cmd.CommandText = @"DELETE FROM items;";
       cmd.ExecuteNonQuery();
       conn.Close();
       if (conn != null)
       {
-        conn.Dispose();
+       conn.Dispose();
       }
     }
-    public static Item Find(int searchId)
-    {
-      Item placeholderItem = new Item("placeholder item");
-      return placeholderItem;
-    }
-
-    public void Save()
-    {
-      MySqlConnection conn = DB.Connection();
-      conn.Open();
-      var cmd = conn.CreateCommand() as MySqlCommand;
-      // Begin new code
-      cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
-      MySqlParameter description = new MySqlParameter();
-      description.ParameterName = "@ItemDescription";
-      description.Value = this.Description; //"this" is an Object of type Item
-      cmd.Parameters.Add(description);    
-      cmd.ExecuteNonQuery();
-      Id = (int) cmd.LastInsertedId;
-      // End new code
-      conn.Close();
-      if (conn != null)
-      {
-        conn.Dispose();
-      }
-    }
-
   }
 }
